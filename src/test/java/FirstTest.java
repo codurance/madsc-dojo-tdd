a677d8b5f41f39284f3f750b51ab39446ece3f6b
Javier Lopez <THO.jlopez@iberia.es>
2024-03-13 08:28:33 +0100

    add junit and a test example

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FirstTest {
+    @Test
+    public void shouldReturnZero() {
+        assertEquals(0, 0);
+    }
+}
